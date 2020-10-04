f32314b73c4d5db30e5e297cab349200e4cead41
Ufuka <61376126+Ufuk-a@users.noreply.github.com>
2020-10-04 15:51:16 +0300

    Add files via upload

--- Paint.py
+++ Paint.py
@@ -4,10 +4,18 @@ pygame.init()
 paint = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Python - Paint")
 
+
+
 def pen():
-    x = pygame.mouse.get_pos()
+    pen_pos = pygame.mouse.get_pos()
     if pygame.mouse.get_pressed()[0] == 1:
-        pygame.draw.circle(paint, (0, 0, 0), x, 5)
+        pygame.draw.circle(paint, (0, 0, 0), pen_pos, 5)
+        pygame.display.update()
+        
+def eraser():
+    esaser_pos = pygame.mouse.get_pos()
+    if pygame.mouse.get_pressed()[2] == 1:
+        pygame.draw.circle(paint, (255, 255, 255), esaser_pos, 10)
         pygame.display.update()
         
 paint.fill((255, 255, 255))
@@ -21,3 +29,4 @@ while painting:
             painting = False
     
     pen()
+    eraser()
